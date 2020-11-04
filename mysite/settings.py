8f363a8d1e3db7afd003db732f96e58be0693d9c
niyzora <niyzora@gmail.com>
2020-11-05 02:55:07 +0600

    Added login

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
+LOGIN_REDIRECT_URL = '/'
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
